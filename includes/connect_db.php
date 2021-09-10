b442cecbdee1f5ba97becd6f31bf057443aacad1
Nayeem Hossain <77660934+im-nayeem@users.noreply.github.com>
2021-09-10 17:43:33 +0600

    Update connect_db.php

--- includes/connect_db.php
+++ includes/connect_db.php
@@ -1,6 +1,8 @@
 <?php
 
-// ---function to connect with database--//
+/** ---function to connect with database--
+* @return object $conn connection to database
+*/
             function getDB()
             {
                $db_host="localhost"; //host server
@@ -22,7 +24,7 @@
             //End of function to connect with database
 
 
-//  an array that contains all of the DB entity name and formal name 
+//  mapping all of the DB entity name to formal name 
    $entity=["name"=>"Name","email"=>"E-mail","department"=>"Department","id"=>"ID","session"=>"Session","vaccinated"=>"Vaccinated","vaccination_id"=>"Vaccination ID","vaccine_name"=>"Vaccine Name","vaccination_date"=>"Vaccination Date","first_dose"=>"First Dose","second_dose"=>"Second Dose","fever"=>"Fever","headache"=>"Headache","vomitting"=>"Vomitting","other_effect"=>"Other Effects"];
 
 
@@ -41,13 +43,17 @@
 
 
 
-
+ //mapping entity with table names
       $tables=["name"=>"student","email"=>"student","department"=>"student","id"=>"student","session"=>"student","vaccinated"=>"student","vaccination_id"=>"vaccination_info","vaccine_name"=>"vaccination_info","vaccination_date"=>"vaccination_info","first_dose"=>"vaccination_info","second_dose"=>"vaccination_info","fever"=>"side_effect","headache"=>"side_effect","vomitting"=>"side_effect","other_effect"=>"side_effect"];
 
 
 
 
-
+/*--------Function to get table elements
+* @param object connection to database
+* @param string $sql sql query 
+* @return array  - all elements found by sql query
+*/
 
       function get_element($conn,$sql)
       {
